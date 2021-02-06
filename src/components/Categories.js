1cbc1094ee6eccb25df567a53157d8cbcd10d7b1
unknown <sirasala.harshachintu10@gmail.com>
2021-02-06 16:49:59 +0530

    product

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -69,7 +69,7 @@ const Categories = () => {
                 </Navbar.Collapse>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto">
-                        <NavDropdown title="Bakery,Cakes&Dairy" id="basic-nav-dropdown">
+                        <NavDropdown  title="Bakery,Cakes&Dairy" id="basic-nav-dropdown">
                             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
